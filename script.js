29c0b54f0dc9f64dff00a64a87808c0bcf9d5949
aakash05mrt <88130639+aakash05mrt@users.noreply.github.com>
2021-10-25 21:39:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo ", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo ", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven ", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes", filePath: "songs/5.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Rabba ", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "Sakhiyaan ", filePath: "songs/1.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Bhula Dena ", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Tumhari Kasam", filePath: "songs/5.mp3", coverPath: "covers/2.jpg"},
-    {songName: "Na Jaana ", filePath: "songs/3.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Baari ", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Dil Kyun ye Mera ", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Kasoor", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Sakhiyaan ", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Tuje Kitna Chahne", filePath: "songs/5.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Dil Kyun ye Mera ", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Baari ", filePath: "songs/1.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Sakhiyaan ", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Tuje Kitna Chahne", filePath: "songs/5.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Kasoor ", filePath: "songs/3.mp3", coverPath: "covers/4.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
